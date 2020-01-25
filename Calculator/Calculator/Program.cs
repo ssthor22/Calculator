57393d5725798c6524754c1bd659156e082a1e9a
Seth <ssthor22@gmail.com>
2020-01-24 19:27:13 -0500

    added all files

---
+++ Calculator/Calculator/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Computation
+{
+    class Calculator
+    {
+        public static void ProcessEq(string equation)
+        {
+            //Parse the equation using regular expression
+            equation = equation.Replace(" ", "");
+            char[] operations = { '+', '-', '*', '/' };
+            string[] parts = Regex.Split(equation, @"(?<=[-+*/()])|(?=[-+*/()])");
+            foreach (var item in parts)
+                Console.WriteLine(item);
+        }
+
+        static void Main()
+        {
+            string usrinput;
+
+            Console.WriteLine("Calculator\n");
+            Console.WriteLine("----------\n");
+            Console.WriteLine("Enter your math equation:\n");
+            usrinput = Console.ReadLine();
+
+            ProcessEq(usrinput);
+        }
+    }
+
+        
+}
